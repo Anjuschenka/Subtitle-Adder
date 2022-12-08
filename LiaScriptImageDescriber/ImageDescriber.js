fb1ec1bfbfe60d22ed8f68d63d6ded6dfeccdd03
Anjuschenka <anjvoi1@web.de>
2022-12-08 21:48:10 +0100

    Transferred Print function successfully

--- LiaScriptImageDescriber/ImageDescriber.js
+++ LiaScriptImageDescriber/ImageDescriber.js
@@ -273,6 +273,109 @@ function PlacePrinter() {
     //todo
 }
 
+function PrintDocument() {
+    let w = window.open();
+    let div_to_print = document.createElement("div");
+    let table = document.createElement("table");
+    let tableRow1 = document.createElement("tr");
+    //table row 1:
+    //image
+    let img = document.createElement("img");
+    img.id = "pr_Img"
+    img.src = ImgUrlLink;
+    let colImage = document.createElement("td");
+    colImage.appendChild(img);
+    
+    //author information
+    let AuthorName = document.createElement("div");
+    AuthorName.innerHTML = document.getElementById("NameBox").value;
+    let MatrNb = document.createElement("div");
+    MatrNb.innerHTML = document.getElementById("MatBox").value;
+
+    let authorInfo = document.createElement("td");  
+    authorInfo.appendChild(document.createTextNode("Name:"));
+    authorInfo.appendChild(AuthorName);
+    authorInfo.appendChild(document.createTextNode("Matrikl Nbr:"));
+    authorInfo.appendChild(MatrNb);
+    
+    colImage.style = "width: 50%"
+    authorInfo.style = "width: 50%"
+    tableRow1.appendChild(colImage);
+    tableRow1.appendChild(authorInfo);
+    //table
+    table.appendChild(tableRow1);
+
+    let tableRow2 = document.createElement("tr");
+    // table row 2:
+    let tb_div = document.createElement("td"); 
+    tb_div.colSpan = 2; 
+    let printKeys = Object.keys(TBcontent);
+    for(let printText of printKeys) {
+        temp = document.createElement("div")
+        let contentText = TBcontent[printText];
+        contentText = contentText.split("\n");
+        for(let txt of contentText) {
+            temp.appendChild(document.createTextNode(txt));
+            temp.appendChild(document.createElement("br"));
+        }
+        tb_div.appendChild(temp);
+        tb_div.appendChild(document.createElement("br"));
+    }
+    tableRow2.appendChild(tb_div);
+    table.appendChild(tableRow2);
+
+    //Add text to the printing file:
+    let textDiv = document.createElement("div");
+
+    function createParagraph(name){
+    let attention = document.getElementById(name);
+    let paragraph = document.createElement("p");
+    console.log("Appending: " + attention.value);
+    paragraph.innerHTML = attention.value;
+
+    //analysis = analysis.concat(attention.value);
+    //analysis = analysis.concat(" \n ");
+    //main.append(paragraph);
+    return paragraph;
+    }
+
+    let printTitle = document.createElement("h3");
+    printTitle.innerHTML = document.getElementById("TitleTextArea").value;
+
+    textDiv.appendChild(printTitle);
+    textDiv.appendChild(createParagraph("AttentionTextArea"));
+    textDiv.appendChild(createParagraph("IntroductionTextArea"));
+    textDiv.appendChild(createParagraph("ExecutionTextArea"));
+    textDiv.appendChild(createParagraph("DetailsTextArea"));
+    textDiv.appendChild(createParagraph("EndTextArea"));
+
+    //Add all divs we want to have printed:
+    
+    div_to_print.appendChild(table);
+    div_to_print.appendChild(textDiv);
+    
+    //console.log(div_to_print.innerHTML);
+    let printContent = div_to_print.innerHTML;
+
+    //w.document.body.appendChild(document.getElementsByClassName("print")[0]);
+
+    w.document.body.appendChild(div_to_print);
+    //w.document.write(document.getElementsByClassName("print")[0].innerH‌​TML);
+    w.print();
+    w.close();
+}
+
+/*function PlacePrinter() {
+    let btn = document.createElement("button");
+    btn.innerHTML = "Print";
+    btn.onclick = function() {
+        PrintDocument();
+    }
+    document.getElementById("Printer").innerHTML = "";
+    document.getElementById("Printer").appendChild(btn);
+    //todo
+}*/
+
 /*function PrintDocument() {
     let div_to_print = document.createElement("div");
     let table = document.createElement("table");
